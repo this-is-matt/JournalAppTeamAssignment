0daf4a5a5ceeaf7fa82d03ef81074240c8d305aa
Quibly <trippy524@yahoo.com>
2022-07-08 09:35:43 -0600

    testing updated

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,8 @@
 const {MongoClient} = require('mongodb');
 require('dotenv/config');
+const {ObjectId} = require('mongodb');
 
-describe('insert', () => {
+describe('requests', () => {
     let connection;
     let db;
 
@@ -17,11 +18,47 @@ describe('insert', () => {
         await connection.close();
     });
 
-    it('should insert a doc into collection', async () => {
+    it('should insert a doc into Entries collection', async () => {
         const entries = db.collection('Entries');
-        const mockEntry = {_id: '62b66cef20fba432c016e910', title: 'Test Title', entry: 'Test Entry'};
+        const id = new ObjectId();
+        const mockEntry = {_id: id, title: 'Test Title', entry: 'Test Entry'};
         await entries.insertOne(mockEntry);
-        const insertedEntry = await entries.findOne({_id: '62b66cef20fba432c016e910'});
+        const insertedEntry = await entries.findOne({_id: id});
         expect(insertedEntry).toEqual(mockEntry);
+    });
+
+    it('should insert a doc into Profile collection', async () => {
+        const profile = db.collection('Profile');
+        const id = new ObjectId();
+        const mockProfile = {_id: id, profileName: 'Test Name', mood: 'Test', interests: 'Test interests', introduction: 'Hi. I am a test.', profileImageUrl: 'https://lh3.googleusercontent.com/a/AATXAJwccRK4PnVWIBXbbwwbjPrEipUl8VWUoRh8fiXn=s96-c'};
+        await profile.insertOne(mockProfile);
+        const insertedProfile = await profile.findOne({_id: id});
+        expect(insertedProfile).toEqual(mockProfile);
+    });
+
+    it('should insert a doc into Themes collection', async () => {
+        const themes = db.collection('Themes');
+        const id = new ObjectId();
+        const mockTheme = {_id: id, title: 'Test Title', createdUsername: 'Test', mainColor: 'Test', secondaryColor: 'Test', backgroundImage: 'https://lh3.googleusercontent.com/a/AATXAJwccRK4PnVWIBXbbwwbjPrEipUl8VWUoRh8fiXn=s96-c', searchTags: 'Test, test', description: 'This is a test.'};
+        await themes.insertOne(mockTheme);
+        const insertedTheme = await themes.findOne({_id: id});
+        expect(insertedTheme).toEqual(mockTheme);
+    });
+
+    it('should insert a doc into Users collection', async () => {
+        function makeid() {
+            let text = "";
+            const possible = "0123456789";
+            for (let i = 0; i < 20; i++)
+              text += possible.charAt(Math.floor(Math.random() * possible.length));
+            return text;
+        }
+        const newGoogleId = makeid();
+        const users = db.collection('Users');
+        const id = new ObjectId();
+        const mockUser = {_id: id, googleId: newGoogleId, displayName: 'Testy Testerson', firstName: 'Te', lastName: 'St', image: 'https://lh3.googleusercontent.com/a/AATXAJwccRK4PnVWIBXbbwwbjPrEipUl8VWUoRh8fiXn=s96-c'};
+        await users.insertOne(mockUser);
+        const insertedUser = await users.findOne({_id: id});
+        expect(insertedUser).toEqual(mockUser);
     });
 });
