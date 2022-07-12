e5f773f4fa862a06626a77250c35973e242b77dc
Quibly <trippy524@yahoo.com>
2022-07-11 18:06:07 -0600

    error correction

--- __tests__/routes.test.js
+++ __tests__/routes.test.js
@@ -6,6 +6,7 @@ describe('routes', () => {
     describe('test all routes', () => {
         describe('given the routes are working and ids exist', () => {
             beforeAll(function () {
+                // create fake call backs for authentication
                 auth.ensureGuest.callsFake((req, res, next) => next());
                 auth.ensureAuth.callsFake((req, res, next) => next());
                 auth.ensureAuth2.callsFake((req, res, next) => next());
@@ -16,21 +17,21 @@ describe('routes', () => {
                 auth.ensureAuth.restore();
                 auth.ensureAuth2.restore();
             });
-            it('Entries collection should return a 200', async () => {
+            it('Entries endpoint should return a 200', async () => {
                const entriesId = '62b66cef20fba432c016e9c9';
                await supertest(app).get(`/entries/${entriesId}`).expect(200);
             });
-            it('Profile collection should return a 200', async () => {
+            it('Profile endpoint should return a 200', async () => {
                 const profileId = '62b66acd4eb18e69d8ed4b5b';
-                await supertest(app).get(`/entries/${profileId}`).expect(200);
+                await supertest(app).get(`/profile/${profileId}`).expect(200);
             });
-            it('Themes collection should return a 200', async () => {
+            it('Themes endpoint should return a 200', async () => {
                 const themesId = '62b66bb057af867560bdfe6e';
-                await supertest(app).get(`/entries/${themesId}`).expect(200);
+                await supertest(app).get(`/themes/${themesId}`).expect(200);
             });
-            it('Users collection should return a 200', async () => {
-                const themesId = '62b64382b7240f4e657039fb';
-                await supertest(app).get(`/entries/${themesId}`).expect(200);
+            it('Users endpoint should return a 200', async () => {
+                const usersId = '62b64382b7240f4e657039fb';
+                await supertest(app).get(`/users/${usersId}`).expect(200);
             });
         });
     });
